580acd4cab388edbbb896c043b752c73b71e62af
William Bojczuk <williambojczuk@gmail.com>
2023-03-10 15:00:34 -0500

    Added uuid gen

--- jsdev.js
+++ jsdev.js
@@ -137,6 +137,15 @@ lazyLoad: (elems, settings)=>{
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
+  },
+
+  GETValues: ()=>{
+    const searchParams = new URLSearchParams(window.location.search);
+    const retval = {}
+    for (const key of searchParams.keys()) {
+        retval[key] = searchParams.get(key);
+      }
+      return retval;
   }
   
   
