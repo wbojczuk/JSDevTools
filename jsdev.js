6dad76bc210a3e45015ea93206b7d2b150f7394d
William Bojczuk <williambojczuk@gmail.com>
2023-03-10 13:37:40 -0500

    Added uuid gen

--- jsdev.js
+++ jsdev.js
@@ -101,11 +101,19 @@ lazyLoad: (elems, settings)=>{
     (jsdev.getElementRefs(elems, {multiple: true})).forEach((elem)=>{
         lazyObserver.observe(elem);
     });
-}
+},
+
+// Credit to https://stackoverflow.com/users/109538/broofa for this v4 uuid generator
+ getUUID: ()=>{
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+  
+  
 
 
 };
-
 
 Object.prototype.isElement = function(){
     let retval = false;
